1785b75b8a52d1ffd6b74302f5baa9dcfd0d006d
Delaram Ghoreishi <delaramq@gmail.com>
2017-09-27 18:16:10 -0400

    Create student_first.cu

---
+++ Problem Sets/Problem Set 5/student_first.cu
@@ -0,0 +1,96 @@
+/* Udacity HW5
+   Histogramming for Speed
+
+   The goal of this assignment is compute a histogram
+   as fast as possible.  We have simplified the problem as much as
+   possible to allow you to focus solely on the histogramming algorithm.
+
+   The input values that you need to histogram are already the exact
+   bins that need to be updated.  This is unlike in HW3 where you needed
+   to compute the range of the data and then do:
+   bin = (val - valMin) / valRange to determine the bin.
+
+   Here the bin is just:
+   bin = val
+
+   so the serial histogram calculation looks like:
+   for (i = 0; i < numElems; ++i)
+     histo[val[i]]++;
+
+   That's it!  Your job is to make it run as fast as possible!
+
+   The values are normally distributed - you may take
+   advantage of this fact in your implementation.
+
+*/
+
+
+#include "utils.h"
+#include "reference.cpp"
+#include <stdio.h>
+
+__global__
+void yourHisto(const unsigned int* const vals, //INPUT
+               unsigned int* const histo,      //OUPUT
+               int numVals,
+               int numBins)
+{
+  //TODO fill in this kernel to calculate the histogram
+  //as quickly as possible
+
+  //Although we provide only one kernel skeleton,
+  //feel free to use more if it will help you
+  //write faster code
+  
+  int index = threadIdx.x + blockIdx.x * blockDim.x;
+  int idx = threadIdx.x;
+  
+  if(index > numVals) return;
+  
+  extern __shared__ unsigned int s_histo[];
+  
+  /*if(idx == 0)
+  {
+    for(int i = 0; i < numBins; i++)
+    {
+       s_histo[i]=0;
+    }
+  }*/
+  
+  s_histo[idx] = 0;
+  __syncthreads();
+  
+  atomicAdd(&s_histo[vals[index]], 1);
+  __syncthreads();
+  
+  /*if(idx == 0)
+  {
+    for(int i = 0; i < numBins; i++)
+    {
+       atomicAdd(&histo[i], s_histo[i]);
+    }
+  }*/
+  
+  atomicAdd(&histo[idx], s_histo[idx]);
+}
+
+void computeHistogram(const unsigned int* const d_vals, //INPUT
+                      unsigned int* const d_histo,      //OUTPUT
+                      const unsigned int numBins,
+                      const unsigned int numElems)
+{
+  //printf("numBin = %d\n",numBins);
+  //printf("numElems = %d\n",numElems);
+  int blockLength = numBins;
+  dim3 threads(blockLength, 1, 1);
+  dim3 blocks(ceil(numElems/blockLength), 1, 1);
+  //TODO Launch the yourHisto kernel
+  yourHisto<<<blocks, threads, (numBins) * sizeof(unsigned int)>>>(d_vals, d_histo, numElems, numBins);
+  //if you want to use/launch more than one kernel,
+  //feel free
+  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+
+  //delete[] h_vals;
+  //delete[] h_histo;
+  //delete[] your_histo;*/
+}
